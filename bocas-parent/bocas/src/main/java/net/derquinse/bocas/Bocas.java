abdb5ec382b071ad68dd13a02759ef38ad10335b
Andres Rodriguez <andresrc@gmail.com>
2012-01-23 06:16:32 +0100

    First version of the interface.

--- bocas-parent/bocas/src/main/java/net/derquinse/bocas/Bocas.java
+++ bocas-parent/bocas/src/main/java/net/derquinse/bocas/Bocas.java
@@ -15,17 +15,53 @@
  */
 package net.derquinse.bocas;
 
+import java.io.InputStream;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 import net.derquinse.common.base.ByteString;
 
 import com.google.common.annotations.Beta;
+import com.google.common.base.Optional;
+import com.google.common.io.InputSupplier;
 
 /**
- * Interface for a BOCAS service. Guava library is expected to provide a ByteString object. The use
- * of the one from derquinse-commons is temporary.
+ * Interface for a BOCAS repository. Guava library is expected to provide a ByteString object. The
+ * use of the one from derquinse-commons is temporary.
  * @author Andres Rodriguez.
  */
 @Beta
 public interface Bocas {
 	/** Returns whether the repository contains the provided key. */
 	boolean contains(ByteString key);
+
+	/** Returns the subset of the provided keys that are contained in the repository. */
+	Set<ByteString> contained(Iterable<ByteString> keys);
+
+	/**
+	 * Returns the object for the provided key.
+	 * @param key The requested key.
+	 * @return The object, if found.
+	 */
+	Optional<InputSupplier<InputStream>> get(ByteString key);
+
+	/**
+	 * Returns the objects for a collection of keys.
+	 * @param keys The requested keys.
+	 * @return The objects found in the repository or an empty map if none is found.
+	 */
+	Map<ByteString, InputSupplier<InputStream>> get(Iterable<ByteString> keys);
+
+	/**
+	 * Puts an object into the repository.
+	 * @return The generated key.
+	 */
+	ByteString put(InputSupplier<? extends InputStream> object);
+
+	/**
+	 * Puts some objects into the repository in a single operation.
+	 * @return The list of generated keys, in the same order than the objects provided.
+	 */
+	List<ByteString> putAll(List<? extends InputSupplier<? extends InputStream>> objects);
 }
